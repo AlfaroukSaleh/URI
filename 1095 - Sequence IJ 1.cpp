fb42c333e7811c45678879b9ed47af8dcbaa4701
AlfaroukSaleh <50967999+AlfaroukSaleh@users.noreply.github.com>
2019-05-23 23:31:20 +0200

    Create 1095 - Sequence IJ 1.cpp

---
+++ 1095 - Sequence IJ 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main (){
+    int j=60;int x=-2;
+    for (int i=1;i<=31;i++){
+        x+=3;
+        cout <<"I="<<x<<" J="<<j<<endl;
+        if (j==0)break;
+j-=5;        
+    
+    
+    }
+
+    
+    
+    
+    return 0;}
