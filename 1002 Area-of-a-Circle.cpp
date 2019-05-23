ff3aa94f77dd3117e60752299509ea073114c7db
AlfaroukSaleh <50967999+AlfaroukSaleh@users.noreply.github.com>
2019-05-23 23:41:39 +0200

    Create 1002 Area-of-a-Circle.cpp

---
+++ 1002 Area-of-a-Circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main (){
+    cout <<fixed<<setprecision(4);
+double n=3.14159;
+double R;
+double A;
+cin>>R;
+A=n*R*R;
+cout << "A="<<A<<endl;
+
+
+}
