51a10416ab976a5385e639960af027c0fa4ac9f4
AlfaroukSaleh <50967999+AlfaroukSaleh@users.noreply.github.com>
2019-05-23 23:34:56 +0200

    Create 1078 - Multiplication Table.cpp

---
+++ 1078 - Multiplication Table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main (){
+int x;
+cin>>x;
+for (int i=1;i<=10;i++){
+    cout<<i<<" x "<<x<<" = "<<x*i<<endl;
+    
+    }
+
+    
+    
+    
+    return 0;}
