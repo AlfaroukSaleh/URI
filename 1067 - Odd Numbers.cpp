384211035f0e93ac2890f49460d330e35323d815
AlfaroukSaleh <50967999+AlfaroukSaleh@users.noreply.github.com>
2019-05-23 23:23:57 +0200

    Create 1067 - Odd Numbers.cpp

---
+++ 1067 - Odd Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include<iomanip>
+using namespace std;
+int main (){
+  
+    int x; 
+    cin>>x;
+    for (int i=1;i<=x;i++){
+        if (i%2)
+            cout<<i<<endl;
+    }
+    return 0;
+
+
+}
